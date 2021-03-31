12aeef8f52727f3a7ca7163f45c19f449864915b
Alejandro Parra <48602979+ajparrah@users.noreply.github.com>
2021-03-31 15:22:15 -0400

    feat(User): Login with JWT (#3)

--- routes/middlewares/userMiddlewares.js
+++ routes/middlewares/userMiddlewares.js
@@ -1,12 +1,24 @@
-const { userSignUpValidator } = require('../../helpers/validate');
+const { userSignUpValidator, userLoginValidator } = require('../../helpers/validate');
 
 const userSignUpMiddlewares = (req, res, next) => {
-  const resultValidation = userSignUpValidator(req.body);
-  if(resultValidation){
+  const validationFailed = userSignUpValidator(req.body);
+  if(validationFailed){
     return res.status(400).json({
       ok: false,
       msg: 'Something wents wrong to sign up',
-      error: resultValidation,
+      error: validationFailed,
+    });
+  }
+  next();
+};
+
+const userLoginMiddlewares = (req, res, next) => {
+  const validationFailed = userLoginValidator(req.body);
+  if(validationFailed){
+    return res.status(400).json({
+      ok: false,
+      msg: 'Something wents wrong to sign in',
+      error: validationFailed,
     });
   }
   next();
@@ -14,4 +26,5 @@ const userSignUpMiddlewares = (req, res, next) => {
 
 module.exports = {
   userSignUpMiddlewares,
+  userLoginMiddlewares,
 }
--- services/UserService.js
+++ services/UserService.js
@@ -5,28 +5,11 @@ class UserService {
     return await User(newUser).save();
   }
 
-  // async getUsers(from, limit) {
-  //   return await User.find({ isActive: true }).skip(from).limit(limit);
-  // }
+  async getUserByEmail(userEmail) {
+    return await User.findOne({
+      email: userEmail,
+    }); // Return null if user doesn't exist
+  }
 
-  // async update(id, userToUpdate) {
-  //   const options = {
-  //     new: true,
-  //     runValidators: true,
-  //     context: 'query', //Es requerido para usar el este metodo y obligar a validar con el mongoose unique validator
-  //   };
-  //   return await User.findByIdAndUpdate(id, userToUpdate, options);
-  // }
-
-  // async delete(id) {
-  //   const userToDelete = {
-  //     isActive: false,
-  //   };
-  //   return await User.findByIdAndUpdate(id, userToDelete, { new: true });
-  // }
-
-  // async getQuantityActiveUsers() {
-  //   return await User.countDocuments({ isActive : true });
-  // }
 }
 module.exports = UserService;
