2fc8db473a1d9ce895cca3d672a6da8653f7e046
Alejandro Parra <alejandro_parra_15@hotmail.com>
2021-03-30 23:15:11 -0400

    feat(env): Initial Setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRouter = require('./routes/userRouter');
+const app = express();
+
+app.use(express.json()); //parse -> application/json
+
+app.use('/users', userRouter);
+
+const port = 8080;
+const optionsMongoose = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose
+  .connect('mongodb://localhost:27017/reachyeti', optionsMongoose)
+  .then(() => console.log('Mongo database is online'))
+  .catch((error) => console.log(error));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
